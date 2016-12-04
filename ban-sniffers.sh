dfc5c81aa1cd55b3f720812608dd124e95dc8f1a
Tony Air <tony@thewebdevelopmentagency.com>
2016-12-04 13:50:33 +0300

    slash escape

--- ban-sniffers.sh
+++ ban-sniffers.sh
@@ -10,7 +10,7 @@ PATHTOREPOSITORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 LISTIP="${PATHTOREPOSITORY}"/conf.d/block-ip.conf
 cat "${PATHTOREPOSITORY}"/conf.d/block-ip.conf | head -n -1 > /tmp/block-ip.conf
-cat /var/logs/nginx/ban-sniffer.log | awk -F " - " 'NF {print $2"/32 1;"}' | sed '/^/32 1;/d' >> /tmp/block-ip.conf
+cat /var/logs/nginx/ban-sniffer.log | awk -F " - " 'NF {print $2"/32 1;"}' | sed '/^\/32 1;/d' >> /tmp/block-ip.conf
 echo "}" >> /tmp/block-ip.conf
 cat /tmp/block-ip.conf | uniq > "${LISTIP}"
 
