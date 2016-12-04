2f0ab51bf3a789cfb015ff847237fbdc482d0518
Tony Air <tony@thewebdevelopmentagency.com>
2016-12-05 02:36:44 +0300

    TO-DO

--- ban-sniffers.sh
+++ ban-sniffers.sh
@@ -12,6 +12,8 @@ LISTIP="${PATHTOREPOSITORY}"/conf.d/block-ip.conf
 cat "${PATHTOREPOSITORY}"/conf.d/block-ip.conf | head -n -1 | tail -n +4 > /tmp/block-ip.conf
 cat /var/logs/nginx/ban-sniffer.log | awk -F " - " 'NF {print $2"/32 1;"}' | sed '/^\/32 1;/d' >> /tmp/block-ip.conf
 
+# TO-DO: check if it's search engine bot
+# check bot: host 66.249.66.1 | tail -1 | grep -oE "(baidu\.jp|googlebot\.com|google\.com|yandex\.com|search\.msn\.com|yahoo\.com|aol\.com|duckduckgo\.com)"
 echo '# WARNING! This file was generated. Do not change!' > "${LISTIP}"
 echo 'geo $block_ip {' >> "${LISTIP}"
 echo 'default 0;' >> "${LISTIP}"
