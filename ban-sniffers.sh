765c8f79f458f0dcc9959183ea09f6345f0b1070
Tony Air <tony@thewebdevelopmentagency.com>
2016-12-04 15:25:45 +0300

    Uniq IP fixture

--- ban-sniffers.sh
+++ ban-sniffers.sh
@@ -9,10 +9,14 @@ PATHTOREPOSITORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 
 LISTIP="${PATHTOREPOSITORY}"/conf.d/block-ip.conf
-cat "${PATHTOREPOSITORY}"/conf.d/block-ip.conf | head -n -1 > /tmp/block-ip.conf
+cat "${PATHTOREPOSITORY}"/conf.d/block-ip.conf | head -n -1 | tail -n +4 > /tmp/block-ip.conf
 cat /var/logs/nginx/ban-sniffer.log | awk -F " - " 'NF {print $2"/32 1;"}' | sed '/^\/32 1;/d' >> /tmp/block-ip.conf
-echo "}" >> /tmp/block-ip.conf
-cat /tmp/block-ip.conf | uniq > "${LISTIP}"
+
+echo '# WARNING! This file was generated. Do not change!' > "${LISTIP}"
+echo 'geo $block_ip {' >> "${LISTIP}"
+echo 'default 0;' >> "${LISTIP}"
+cat /tmp/block-ip.conf | sort | uniq >> "${LISTIP}"
+echo '}' >> "${LISTIP}"
 
 rm /tmp/block-ip.conf
 #rm /var/logs/nginx/ban-sniffer.log
